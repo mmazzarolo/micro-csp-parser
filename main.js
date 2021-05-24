b7befefdb9c3ebc5659d2f3c6874d65f1a526af9
mmazzarolo <matteomazzarolo@invisionapp.com>
2021-05-24 14:29:15 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,25 @@
+import style from './style.css'
+
+const inputTextarea = document.querySelector('#input-textarea');
+const inputUsePlainText = document.querySelector('#input-use-plain-text');
+const inputButton = document.querySelector('#input-button');
+const output = document.querySelector('#output');
+
+inputButton.addEventListener('click', parseContentSecurityPolicy);
+
+function parseContentSecurityPolicy () {
+  const items = input.value.split(';').filter(item => !!item).map(item => item.trim()).map(item => ({
+    name: item.substr(0, item.indexOf(' ')),
+    values: item.substr(item.indexOf(' ') + 1, item.length).split(' ') 
+  }))
+  if (inputUsePlainText.checked) {
+    output.innerHTML = items.map(item => 
+      `<b>${item.name}</b><br /><code>${item.values.join(' ')}</code><br />`
+      ).join('<br />')
+  } else {
+    output.innerHTML = items.map(item => 
+      `<b>${item.name}</b><br /><ul>${item.values.map((x) => `<li><code>${x}</code></li>`).join('')}</ul>`
+    ).join('<br />')
+  }
+  window.items = items;
+}
